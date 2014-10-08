4deb54b3410d5cc992a87766a3efc04d45c95d9c
Ian Miller <irmiller22@gmail.com>
2014-10-08 17:30:09 -0400

    updated configurations

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-# A sample Gemfile
 source "https://rubygems.org"
 
-# gem "rails"
+gem 'pry', '~> 0.10.1'
+gem 'rspec', '~> 3.1.0'
--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,5 @@
 require_relative '../lib/dog'
 require_relative '../lib/owner'
+
+require 'bundler/setup'
+Bundler.require(:default, :development)
